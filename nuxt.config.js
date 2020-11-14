3db8af8c74e1ab8889d64f226522ad9323a90534
Shuangbing <he.shuangbing@gmail.com>
2020-11-15 00:21:02 +0900

    add firebase config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,9 @@
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
-
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'iclass-teacher-frontend',
+    title: 'iClass.buzz [Teacher]',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
